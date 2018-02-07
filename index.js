6181b865f5105e2c19c4334145d3c99bce386258
Tyler Long <tyler4long@gmail.com>
2018-02-07 15:26:10 +0800

    dotenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const RingCentral = require('ringcentral-js-concise')
+const dotenv = require('dotenv')
 
-const token = { 'access_token': 'U0pDMTFQMDFQQVMwMHxBQURsSW80YjFNbEZfWXE5LUc4NW9PMDBTQkNKbkNSQzBVeTBJbms2YjM4bmQzVm1tOUROYlNUVmtMeHpkRi1QcE4xNF9QWWRPMEVYNENYQjd4dmJsWHJocllOQ2dpZG5FNWVlWmlSNGNzOWtHa2Z3d3Y1REJOTmwzY0k0R09memlkcXdOOS1MR2NtaWIxUmZzSnVzeUttanZzT0dCZ3MwSVFaM0NjbXBxYTdUVkZWYXZxaGxEU2cyUmljQ1EyNzh5V2d8LWtVdDF3fHpYMjlWdnFlczVYR1k3R3pnS1FVckE', 'token_type': 'bearer', 'expires_in': 2147483647, 'scope': 'SubscriptionWebhook Glip', 'owner_id': '235694004', 'endpoint_id': '81UZfQvUS3eQzLHudXG4yw' }
-
+dotenv.config()
+const token = JSON.parse(process.env.token)
 const rc = new RingCentral('', '', RingCentral.SANDBOX_SERVER)
 rc.token(token)
 
